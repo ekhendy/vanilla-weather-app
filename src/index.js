aa8f12430312258aaec75481e53f5c765385306e
ekhendy <ekhendy98@gmail.com>
2021-06-21 20:59:11 +0100

    api forecast url added

--- src/index.js
+++ src/index.js
@@ -49,24 +49,9 @@ function showTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
-
-function citySearch(city) {
-  let apiKey = "55aafc03209b22601e8e8c1b2d96bad0";
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-
-  axios.get(`${apiUrl}${city}&units=metric&appid=${apiKey}`).then(showTemp);
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  citySearch(cityInputElement.value);
-}
-
-let form = document.querySelector("#city-search");
-form.addEventListener("submit", handleSubmit);
-
-citySearch("New York");
 
 function displayC(event) {
   event.preventDefault();
@@ -86,3 +71,46 @@ let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", displayF);
 
 let celsiusTemp = null;
+
+function citySearch(city) {
+  let apiKey = "55aafc03209b22601e8e8c1b2d96bad0";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+  axios.get(`${apiUrl}${city}&units=metric&appid=${apiKey}`).then(showTemp);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  citySearch(cityInputElement.value);
+}
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", handleSubmit);
+
+citySearch("Guildford");
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="grid">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="row cols-3" style="width: 20rem">
+              <div class="col" id=forecast-day>${day}</div>
+              <div class="col" id=forecast-temp>13°</div>
+              <div class="col" id=forecast-icon><img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width=75 /></div>
+            </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "55aafc03209b22601e8e8c1b2d96bad0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
